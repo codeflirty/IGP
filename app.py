1ad130af945368a3668643006a20463e2dc3a7bc
codeflirty <66324181+codeflirty@users.noreply.github.com>
2022-08-04 12:50:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,7 @@ from numpy import array
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 from prettytable import PrettyTable
-from tensorflow.keras.layers import LSTM
-from tensorflow.keras.layers import Dense
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
@@ -89,14 +86,8 @@ def predict():
     X_train, y_train = create_dataset(data, time_step)
 
     if(model=="0"):
-        X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
-        model =Sequential()
-        model.add(LSTM(50, return_sequences=True, input_shape=(time_step,1)))
-        model.add(LSTM(50, return_sequences=True)) 
-        model.add(LSTM(50))
-        model.add(Dense(1))
-        model.compile(loss='mean_squared_error',optimizer='adam')
-        model.fit(X_train, y_train, epochs=5, batch_size=64, verbose=1)
+        model = RandomForestRegressor()
+        model.fit(X_train, y_train)
     
         # Forecasting
         nexta = int(next)
@@ -112,25 +103,25 @@ def predict():
             if(len(temp_input) > 100):
                 x_input = np.array(temp_input[1:])
                 x_input = x_input.reshape(1,-1)
-                x_input = x_input.reshape((1, n_steps,1))
+                x_input = x_input.reshape((1, n_steps))
                 yhat = model.predict(x_input)
-                temp_input.extend(yhat[0].tolist())
+                temp_input.extend(yhat.tolist())
                 temp_input = temp_input[1:]
                 output.extend(yhat.tolist())
                 i=i+1
             else:
-                x_input = x_input.reshape((1, n_steps,1))
+                x_input = x_input.reshape((1, n_steps))
                 yhat = model.predict(x_input)
-                temp_input.extend(yhat[0].tolist())
+                temp_input.extend(yhat.tolist())
                 output.extend(yhat.tolist())
                 i=i+1
-        
+         
         plt.figure()
         plt.rcParams["figure.figsize"] = (14,5)
         day_new = np.arange(1,len(data)+1)
         day_pred = np.arange(len(data)+1,len(data)+1+nexta)
         plt.plot(day_new,scaler.inverse_transform(data).reshape(-1))
-        plt.plot(day_pred,scaler.inverse_transform(output))
+        plt.plot(day_pred,scaler.inverse_transform(np.array(output).reshape(-1, 1)))
         plt.legend(["Historical Data", "Predictions"])
         plt.ylabel("Closing Price (INR)")
         plt.xlabel("Days")
